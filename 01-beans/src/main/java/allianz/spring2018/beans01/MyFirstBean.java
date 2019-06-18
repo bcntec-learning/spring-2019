6f57dbe65965ef55e5a74879e8c7281dcb67ad7f
fphilip <fphilip@houseware.es>
2019-06-18 12:39:26 +0200

    init2

--- 01-beans/src/main/java/allianz/spring2018/beans01/MyFirstBean.java
+++ 01-beans/src/main/java/allianz/spring2018/beans01/MyFirstBean.java
@@ -1,13 +1,54 @@
 package allianz.spring2018.beans01;
 
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.Serializable;
 
 
 @Component
-public class MyFirstBean {
+public class MyFirstBean implements BeanNameAware , BeanPostProcessor {
+    private String myName;
+
 
     public void hello(){
-        System.out.println("hello!!!!!!");
+        System.err.println("hello " + myName + "!!!!!!");
+    }
+
+
+    @Override
+    public void setBeanName(String name) {
+        this.myName=name;
+    }
+
+    /* see MyFirstAOPBean
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof Serializable){
+        return bean;
+        }
+        throw new BeanCreationException("no eres serializable");
+    }
+     */
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @PostConstruct
+    public void p0(){
+        System.err.println("constructed "+myName);
+    }
+
+    @PreDestroy
+    public void p1(){
+        System.err.println("destroying "+myName);
     }
 }
--- 01-beans/src/main/java/allianz/spring2018/beans01/MyFirstContext.java
+++ 01-beans/src/main/java/allianz/spring2018/beans01/MyFirstContext.java
@@ -1,9 +1,21 @@
 package allianz.spring2018.beans01;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ClassUtils;
 
 @Configuration
-@ComponentScan("allianz.spring2018.beans01")
+@ComponentScan(value = "allianz.spring2018.beans01",nameGenerator = MyFirstContext.MyBeanNameGenerator.class)
 public class MyFirstContext {
+
+    static class MyBeanNameGenerator implements BeanNameGenerator {
+
+        @Override
+        public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
+            return ClassUtils.getShortName(definition.getBeanClassName())+"!plus";
+        }
+    }
 }
