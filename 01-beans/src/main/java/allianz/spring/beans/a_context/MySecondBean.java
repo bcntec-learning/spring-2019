42ea0af5e219956a4a3ed941fc76cc3b1e1c81c7
fphilip <fphilip@houseware.es>
2019-10-15 10:58:05 +0200

    correccion

--- 01-beans/src/main/java/allianz/spring/beans/a_context/MySecondBean.java
+++ 01-beans/src/main/java/allianz/spring/beans/a_context/MySecondBean.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MySecondBean {
     //@Autowired que es igual a @Inject
-    private  FirstBean myFirstBean;
+    private  MyFirstBean myFirstBean;
 
 
 
