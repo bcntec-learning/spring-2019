0067ce63a5ac20f4f81d0b5c3fb5dfd9beb8a587
fphilip <fphilip@houseware.es>
2019-10-17 14:50:07 +0200

    test repository

---
+++ 04-data/src/test/java/allianz/spring/data/b_jpa_repository/test/TestRepository.java
@@ -0,0 +1,38 @@
+package allianz.spring.data.b_jpa_repository.test;
+
+import allianz.spring.data.b_jpa_repository.UserRepository;
+import allianz.spring.data.entities.UserEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ComponentScan(basePackageClasses = {UserEntity.class, UserRepository.class})
+@EntityScan(basePackageClasses = UserEntity.class)
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class TestRepository {
+    @Autowired
+    UserRepository repository;
+
+    @Test
+    public void assert_not_null(){
+        assertNotNull(repository);
+    }
+    @Test
+    public void assert_first(){
+        Optional<UserEntity> f = repository.findById(1L);
+        UserEntity u = f.get();
+        assertEquals(u.getName(), "Juan");
+    }
+}
